786e821d1521a35c131097d76864b6b8e68046e6
Dalenson <dal.colares@hotmail.com>
2022-08-28 17:48:07 -0300

    commit

--- service.js
+++ service.js
@@ -66,7 +66,7 @@ function TempoService(Restangular, $http){
     
     this.meuip = function(){
         var req = $http({method: 'GET',
-                            url:'http://ip-api.com/json',
+                            url:'https://api.ipify.org/',
                             headers:{
                                 "Content-Type": "application/json",
                                 "Access-Control-Allow-Origin": "*",
