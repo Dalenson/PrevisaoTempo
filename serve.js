c5d94ffe93662ca19461202ec38071ea5e4e2413
Dale <dal.colares@gmail.com>
2025-05-01 13:03:35 -0300

    Ajuste de publicacao

--- serve.js
+++ serve.js
@@ -1,11 +1,80 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const axios = require("axios");
 
 app.use(express.static(__dirname));
 
 app.get("/", function(req, res){
   res.sendFile(__dirname+'/index.html');
+});
+
+app.get("/meuip", async function(req, res) {
+  try {
+    const response = await axios.get("https://api.ipify.org?format=json");
+    res.json(response.data); // Retorna: { ip: "xxx.xxx.xxx.xxx" }
+  } catch (error) {
+    console.error("Erro ao obter IP:", error);
+    res.status(500).json({ erro: "Não foi possível obter o IP" });
+  }
+});
+
+app.get("/tempo", async (req, res) => {
+  const { woeid } = req.query;
+  if (!woeid) return res.status(400).json({ erro: "WOEID não informado" });
+
+  try {
+    const response = await axios.get("https://api.hgbrasil.com/weather", {
+      params: { woeid }
+    });
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ erro: "Erro ao consultar tempo" });
+  }
+});
+
+app.get("/cidade", async (req, res) => {
+  const { ip } = req.query;
+  if (!ip) return res.status(400).json({ erro: "IP não informado" });
+
+  try {
+    const response = await axios.get("https://api.hgbrasil.com/weather?key=4a5ef733&user_ip="+ip);
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ erro: "Erro ao consultar cidade" });
+  }
+});
+
+app.get("/geoip", async (req, res) => {
+  const { ip } = req.query;
+  if (!ip) return res.status(400).json({ erro: "IP não informado" });
+
+  try {
+    console.log(ip)
+    const response = await axios.get("https://api.hgbrasil.com/geoip?key=4a5ef733&address='"+ip, {params:{"address":ip}});
+    console.log(response.data)
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ erro: "Erro ao consultar GeoIP" });
+  }
+});
+
+app.get("/imagens", async (req, res) => {
+  const page = Math.floor(Math.random() * (10000 - 1)) + 1;
+  try {
+    const response = await axios.get("https://api.pexels.com/v1/curated", {
+      headers: {
+        Authorization: "563492ad6f91700001000001ef09a161ef5d4cffb94691717af73edd"
+      },
+      params: {
+        query: "clouds",
+        per_page: page
+      }
+    });
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ erro: "Erro ao buscar imagens" });
+  }
 });
 
 app.listen(process.env.PORT || 5000);
--- service.js
+++ service.js
@@ -9,69 +9,29 @@ TempoService.$inject = [
 
 function TempoService(Restangular, $http){
     this.consultaTempoCidade = function(id){
-        var req = $http({ method: 'GET',
-                        url: 'https://corsproxy.io/?https://api.hgbrasil.com/weather?woeid='+id,
-                        headers:{
-                            "Content-Type": "application/json",
-                            "Access-Control-Allow-Origin": "*"
-                        }}).then(function(value){
+        var req = $http.get('/tempo?woeid=' + id).then(function(value){
                             return value
                         })
         return req;
     }
 
     this.consultaCidade = function(ip){
-        var req = $http({ method: 'GET',
-                        url: 'https://corsproxy.io/?https://api.hgbrasil.com/weather?key=4a5ef733&user_ip='+ip,
-                        headers:{
-                            "Content-Type": "application/json",
-                            "Access-Control-Allow-Origin": "*"
-                        }}).then(function(value){
+        var req = $http.get('/cidade?ip=' + ip.ip).then(function(value){
                             return value
                         })   
-        // req = {
-        //     cid: '',
-        //     city: 'Buritis, MG',
-        //     city_name: "Buritis",
-        //     condition_code: "27",
-        //     condition_slug: "clear_night",
-        //     currently: "noite",
-        //     date: "05/06/2022",
-        //     description: "Tempo nublado",
-        //     humidity: 57,
-        //     img_id: "27n",
-        //     sunrise: "06:27 am",
-        //     sunset: "05:41 pm",
-        //     temp: 19,
-        //     time: "21:47",
-        //     wind_speedy: "0.86 km/h"
-        // }   
         return req;
     }
 
    
     this.geoIp = function(ip){
-        var req = $http({method: 'GET',
-                    url: 'https://corsproxy.io/?https://api.hgbrasil.com/geoip?key=4a5ef733&address='+ip,
-                    headers:{
-                        "Content-Type": "application/json",
-                        "Access-Control-Allow-Origin": "*",
-                        "address":"177.66.141.88",
-                        "precision":"true"
-                    }}).then(function(value){
+        var req = $http.get('/geoip?ip=' + ip.ip).then(function(value){
                         return value
                     })   
             return req;     
     }
     
     this.meuip = function(){
-        var req = $http({method: 'GET',
-                    url:'https://corsproxy.io/?https://api.ipify.org/',
-                    headers:{
-                        "Content-Type": "application/json",
-                        "Access-Control-Allow-Origin": "*",
-                    }
-                }).then(function(value){
+        var req = $http.get('/meuip').then(function(value){
                     return value
                 })
         return req;
@@ -79,14 +39,7 @@ function TempoService(Restangular, $http){
     
     this.images = function(){
         var page = Math.floor(Math.random() * (10000 - 1)) + 1;
-        var req = $http({method: 'GET',
-            url:'https://corsproxy.io/?https://api.pexels.com/v1/curated?query=clouds&per_page='+page,
-                headers:{
-                    "Content-Type": "application/json",
-                    "Access-Control-Allow-Origin": "*",
-                    "Authorization": "563492ad6f91700001000001ef09a161ef5d4cffb94691717af73edd"
-                }
-            }).then(function(value){
+        var req = $http.get('/imagens').then(function(value){
                 return value
             })
         return req;
